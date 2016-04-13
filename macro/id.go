f9c8118a2173f88e07511e1dee82150d2e4df96b
wd <wd@suboat.com>
2016-04-13 11:59:44 +0800

    feat: some macro

---
+++ macro/id.go
@@ -0,0 +1,24 @@
+package macro
+
+import "strings"
+
+const EMPTY_ID string = "00000000-0000-0000-0000-000000000000"
+
+func IsEmpty(id string) bool {
+	return strings.EqualFold(id, EMPTY_ID)
+}
+
+func ToId(id string) string {
+	return strings.ToLower(strings.TrimSpace(id))
+}
+
+func ValidId(id string) bool {
+	if IsEmpty(id) {
+		return false
+	} else if len(id) == 32 {
+		return true
+	} else if len(id) == 36 && strings.Count(id, "-") == 4 {
+		return true
+	}
+	return false
+}
---
+++ macro/user.go
@@ -0,0 +1,17 @@
+package macro
+
+const MIN_LEN_USERNAME int = 4
+const MAX_LEN_USERNAME int = 64
+
+func ValidUsername(name string) bool {
+	l := len(name)
+	return (l >= MIN_LEN_USERNAME && l <= MAX_LEN_USERNAME)
+}
+
+const MIN_LEN_PASSWORD int = 6
+const MAX_LEN_PASSWORD int = 32
+
+func ValidPassword(pwd string) bool {
+	l := len(pwd)
+	return (l >= MIN_LEN_PASSWORD && l <= MAX_LEN_PASSWORD)
+}
