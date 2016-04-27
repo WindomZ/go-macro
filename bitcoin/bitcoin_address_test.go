6523ef2916f578d6baa9547b3e4a4b6c88826541
WindomZ <zwdsdtc@gmail.com>
2016-04-27 22:46:30 +0800

    feat: random bitcoin address

---
+++ bitcoin/bitcoin_address_test.go
@@ -0,0 +1,33 @@
+package bitcoin
+
+import "testing"
+
+func TestGenerateBitCoinAddress(t *testing.T) {
+	m := make(map[string]bool)
+	for i := 0; i < 100; i++ {
+		address := GenerateBitCoinAddress()
+		//t.Log(address)
+		if _, ok := m[address]; ok {
+			t.Fatalf("Duplicate address: %v", address)
+		}
+		m[address] = true
+		if !VerifyBitCoinAddress(address) {
+			t.Fatalf("Invalid address: %v", address)
+		}
+	}
+}
+
+func TestGenerateBitCoinAddressWith(t *testing.T) {
+	m := make(map[string]bool)
+	for i := 0; i < 100; i++ {
+		address := GenerateBitCoinAddressWith([]byte("ABC"))
+		//t.Log(address)
+		if _, ok := m[address]; ok {
+			t.Fatalf("Duplicate address: %v", address)
+		}
+		m[address] = true
+		if !VerifyBitCoinAddress(address) {
+			t.Fatalf("Invalid address: %v", address)
+		}
+	}
+}
