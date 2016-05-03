955315fbdd5a6080b965b8a9027de3dfe3df7a38
WindomZ <zwdsdtc@gmail.com>
2016-05-03 19:43:45 +0800

    fix: init

---
+++ init.go
@@ -0,0 +1,9 @@
+package go_macro
+
+import (
+	_ "github.com/WindomZ/go-macro/bitcoin"
+	_ "github.com/WindomZ/go-macro/def"
+	_ "github.com/WindomZ/go-macro/http"
+	_ "github.com/WindomZ/go-macro/macro"
+	_ "github.com/WindomZ/go-macro/uuid"
+)
