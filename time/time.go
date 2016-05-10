538dc80a888d28a936e49c7ec44c63c1124ccee0
WindomZ <zwdsdtc@gmail.com>
2016-05-10 13:31:20 +0800

    fix: rename time functions

--- time/time.go
+++ time/time.go
@@ -2,26 +2,31 @@ package gomacro
 
 import "time"
 
-func firstTime(year int, month time.Month, day int) time.Time {
+func DateFirstTime(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 }
 
-func lastTime(year int, month time.Month, day int) time.Time {
+func DateLastTime(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 23, 59, 59, 999999999, time.Local)
 }
 
-func DateFirstTime() time.Time {
-	return firstTime(time.Now().Date())
+func DateTodayFirstTime() time.Time {
+	return DateFirstTime(time.Now().Date())
 }
 
-func DateLastTime() time.Time {
-	return lastTime(time.Now().Date())
+func DateTodayLastTime() time.Time {
+	return DateLastTime(time.Now().Date())
+}
+
+func DateTodayTime(hour, min, sec, nsec int) time.Time {
+	year, month, day := time.Now().Date()
+	return time.Date(year, month, day, hour, min, sec, nsec, time.Local)
 }
 
 func DateNextDayFirstTime() time.Time {
-	return firstTime(time.Now().AddDate(0, 0, 1).Date())
+	return DateFirstTime(time.Now().AddDate(0, 0, 1).Date())
 }
 
 func DateNextDayLastTime() time.Time {
-	return lastTime(time.Now().AddDate(0, 0, 1).Date())
+	return DateLastTime(time.Now().AddDate(0, 0, 1).Date())
 }
--- time/time_test.go
+++ time/time_test.go
@@ -5,14 +5,19 @@ import (
 	"time"
 )
 
-func TestDateFirstTime(t *testing.T) {
+func TestDateTodayFirstTime(t *testing.T) {
 	t.Logf("Now: %v", time.Now())
-	t.Logf("Time: %v", DateFirstTime())
+	t.Logf("Time: %v", DateTodayFirstTime())
 }
 
-func TestDateLastTime(t *testing.T) {
+func TestDateTodayLastTime(t *testing.T) {
 	t.Logf("Now: %v", time.Now())
-	t.Logf("Time: %v", DateLastTime())
+	t.Logf("Time: %v", DateTodayLastTime())
+}
+
+func TestDateTodayTime(t *testing.T) {
+	t.Logf("Now: %v", time.Now())
+	t.Logf("Time: %v", DateTodayTime(12, 22, 23, 0))
 }
 
 func TestDateNextDayFirstTime(t *testing.T) {
