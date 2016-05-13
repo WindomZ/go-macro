004febda8dd668e02eb647488f0dde8ca6e5cb74
WindomZ <zwdsdtc@gmail.com>
2016-05-13 18:09:14 +0800

    feat: new captcha storer

---
+++ captcha/storer.go
@@ -0,0 +1,28 @@
+package captcha
+
+import "time"
+
+var _store Store
+
+func InitStore(collectNum int, expiration time.Duration) {
+	_store = NewMemoryStore(collectNum, expiration)
+}
+
+func GetStore() Store {
+	if _store == nil {
+		InitStore(100, 10*time.Minute)
+	}
+	return _store
+}
+
+func Save(id string, str string) {
+	GetStore().Set(id, []byte(str))
+}
+
+func Fetch(id string, clear bool) string {
+	b := GetStore().Get(id, clear)
+	if b == nil {
+		return ""
+	}
+	return string(b)
+}
