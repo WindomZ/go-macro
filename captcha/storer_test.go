ba7f35cdcf3d7b2b95b3774c32f98a7a153ac9cd
WindomZ <zwdsdtc@gmail.com>
2016-05-13 18:09:21 +0800

    test: captcha storer

---
+++ captcha/storer_test.go
@@ -0,0 +1,16 @@
+package captcha
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSaveFetch(t *testing.T) {
+	id := "captcha id"
+	d := string(randomDigits(10))
+	Save(id, d)
+	d2 := Fetch(id, false)
+	if !strings.EqualFold(d, d2) {
+		t.Errorf("saved %v, randomDigits returned got %v", d, d2)
+	}
+}
