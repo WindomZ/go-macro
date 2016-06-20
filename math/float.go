c8ba3fd2e85dc2de61dcaa488727a8f94752f569
WindomZ <zwdsdtc@gmail.com>
2016-06-20 11:25:23 +0800

    feat: math round func

--- math/float.go
+++ math/float.go
@@ -1,6 +1,9 @@
 package wdmath
 
-import "math"
+import (
+	"github.com/shopspring/decimal"
+	"math"
+)
 
 func FloatPrecision(f float64, prec int, round bool) float64 {
 	pow10_n := math.Pow10(prec)
@@ -8,4 +11,18 @@ func FloatPrecision(f float64, prec int, round bool) float64 {
 		return math.Trunc((f+0.5/pow10_n)*pow10_n) / pow10_n
 	}
 	return math.Trunc((f)*pow10_n) / pow10_n
+}
+
+func FloatRound(val float64, prec int) float64 {
+	pow := math.Pow10(prec)
+	digit := pow * val
+	if _, div := math.Modf(digit); div >= .5 {
+		return math.Ceil(digit) / pow
+	}
+	return math.Floor(digit)
+}
+
+func FloatFixed(value float64, places int) float64 {
+	f, _ := decimal.NewFromFloat(value).Round(int32(places)).Float64()
+	return f
 }
--- math/float_test.go
+++ math/float_test.go
@@ -3,7 +3,25 @@ package wdmath
 import "testing"
 
 func TestFloatPrecision(t *testing.T) {
-	var f float64 = 2.56789
+	var f float64 = 2.012345
 	t.Log(f)
-	t.Log(FloatPrecision(f, 2, true))
+	t.Log(FloatPrecision(f, 5, true))
+}
+
+func TestFloatRound(t *testing.T) {
+	var f float64 = 2.012345
+	t.Log(f)
+	t.Log(FloatRound(f, 5))
+}
+
+func TestFloatFixed(t *testing.T) {
+	var f float64 = 2.012345
+	t.Log(f)
+	t.Log(FloatFixed(f, 5))
+}
+
+func TestFloat(t *testing.T) {
+	t.Log(1.012345, FloatPrecision(1.012345, 5, true), FloatRound(1.012345, 5), FloatFixed(1.012345, 5))
+	t.Log(2.012345, FloatPrecision(2.012345, 5, true), FloatRound(2.012345, 5), FloatFixed(2.012345, 5))
+	t.Log(3.012345, FloatPrecision(3.012345, 5, true), FloatRound(3.012345, 5), FloatFixed(3.012345, 5))
 }
