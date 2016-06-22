5711b27e933c1c0e3fbbfd3fb19f9a78ba1fcca9
WindomZ <zwdsdtc@gmail.com>
2016-06-22 16:59:37 +0800

    feat: json parser

--- json/decode.go
+++ json/decode.go
@@ -1,10 +1,29 @@
 package gojson
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/buger/jsonparser"
+)
 
 func Unmarshal(data []byte, v interface{}) error {
 	if o, ok := v.(JSON); ok {
 		return json.Unmarshal(data, o.JSONUnmarshal())
 	}
 	return json.Unmarshal(data, v)
+}
+
+func JSONGetString(data []byte, keys ...string) (string, error) {
+	return jsonparser.GetString(data, keys...)
+}
+
+func JSONGetFloat(data []byte, keys ...string) (float64, error) {
+	return jsonparser.GetFloat(data, keys...)
+}
+
+func JSONGetInt(data []byte, keys ...string) (int64, error) {
+	return jsonparser.GetInt(data, keys...)
+}
+
+func JSONGetBoolean(data []byte, keys ...string) (bool, error) {
+	return jsonparser.GetBoolean(data, keys...)
 }
